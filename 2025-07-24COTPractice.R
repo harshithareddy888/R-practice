cf3d481a4a5146b77abcfdc8ec8f1d58b35811d1
Harshitha reddy <pannalaharshithareddy@gmail.com>
2025-07-24 02:22:13 +0530

    Add files via upload

---
+++ 2025-07-24COTPractice.R
@@ -0,0 +1,12 @@
+first_name <- c("John", "Rob", "Rachel", "Christy", "Johnson", "Candace", "Carlson", "Pansy", "Darius", "Claudia")
+
+last_name <- c("Mendes", "Stewart", "Abrahamson", "Hickman", "Harper", "Miller", "Landy", "Jordan", "Berry", "Garcia")
+
+job_title <- c("Professional", "Programmer", "Management", "Clerical", "Developer", "Programmer", "Management", "Clerical", "Developer", "Programmer")
+
+employee <- data.frame(id, first_name, last_name, job_title)
+
+penguins %>%
+  mutate(body_mass_kg=body_mass/100)
+
+print(employee)
